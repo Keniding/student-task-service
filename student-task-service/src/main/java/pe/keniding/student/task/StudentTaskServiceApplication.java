83743146e35b5d52bebdce3693b604007b44edb7
henry-keniding <115328041+Keniding@users.noreply.github.com>
2026-01-09 13:08:41 -0500

    Initial commit

---
+++ student-task-service/src/main/java/pe/keniding/student/task/StudentTaskServiceApplication.java
@@ -0,0 +1,13 @@
+package pe.keniding.student.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentTaskServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentTaskServiceApplication.class, args);
+	}
+
+}
